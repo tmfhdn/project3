713142334ff2ed32c9f0b68cc4eeeced71399318
tmfhdn <104114503+tmfhdn@users.noreply.github.com>
2022-06-23 02:38:46 +0900

    6/23

--- js/b.js
+++ js/b.js
@@ -1,16 +1,13 @@
-
-
-
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev"
-    }
-  });
+        var item6 = new item6(".myitem6", {
+            slidesPerView: 1,
+            spaceBetween: 30,
+            loop: true,
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+        });
