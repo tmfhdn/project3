786687871d9a58a0829325d7f3faf9278fc1cd89
tmfhdn <104114503+tmfhdn@users.noreply.github.com>
2022-06-23 03:16:13 +0900

    06/23

--- js/b.js
+++ js/b.js
@@ -11,3 +11,20 @@
                 prevEl: ".swiper-button-prev",
             },
         });
+// ------------------메인슬라이드-----------------------------
+      var swiper = new Swiper(".mySwiper", {
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+          delay: 2500,
+          disableOnInteraction: false,
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
