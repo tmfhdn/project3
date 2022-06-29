e69e1adc86119ee113f8cbbcbfbd75a114484872
tmfhdn <104114503+tmfhdn@users.noreply.github.com>
2022-06-29 09:11:28 +0900

    6/29

--- js/b.js
+++ js/b.js
@@ -1,3 +1,32 @@
+
+
+
+let horizontalBar = document.getElementById("horizontal-underline");
+let horizontalMenus = document.querySelectorAll("nav:first-child a");
+let verticalMenus = document.querySelectorAll("nav:nth-child(2) a");
+
+function verticalIndicator(e) {
+  verticalBar.style.left = e.offsetLeft + "px";
+  verticalBar.style.width = e.offsetWidth + "px";
+  verticalBar.style.top = e.offsetTop + e.offsetHeight + "px";
+}
+
+function horizontalIndicator(e) {
+  horizontalBar.style.left = e.offsetLeft + "px";
+  horizontalBar.style.width = e.offsetWidth + "px";
+  horizontalBar.style.top = e.offsetTop + e.offsetHeight + "px";
+}
+
+horizontalMenus.forEach((menu) =>
+  menu.addEventListener("click", (e) =>
+    horizontalIndicator(e.currentTarget)
+  )
+);
+
+verticalMenus.forEach((menu) =>
+  menu.addEventListener("click", (e) => verticalIndicator(e.currentTarget))
+);
+//----------------메뉴따라다니는컬러---------------------- 
 var currentScrollTop = 0;
 $(document).ready(function () {
   scrollController();
@@ -5,6 +34,7 @@ $(document).ready(function () {
     scrollController();
   });
 });
+
 function scrollController() {
   currentScrollTop = $(window).scrollTop();
   if (currentScrollTop > 100) {
@@ -17,32 +47,32 @@ function scrollController() {
 }
 // -----------------------------상메인------------------------
 var item6 = new item6(".myitem6", {
-            slidesPerView: 1,
-            spaceBetween: 30,
-            loop: true,
-            pagination: {
-                el: ".swiper-pagination",
-                clickable: true,
-            },
-            navigation: {
-                nextEl: ".swiper-button-next",
-                prevEl: ".swiper-button-prev",
-            },
-        });
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 // ------------------메인슬라이드-----------------------------
-      var swiper = new Swiper(".mySwiper", {
-        spaceBetween: 30,
-        centeredSlides: true,
-        autoplay: {
-          delay: 2500,
-          disableOnInteraction: false,
-        },
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-      });
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
